1d3201bff4d9ee705e0a75126db2852b0bc154b0
aparolfe <aparolfe@users.noreply.github.com>
2015-12-22 20:46:38 -0500

    Create chart button generates Stitches panel displaying only stitches from the pattern

--- src/main.js
+++ src/main.js
@@ -1,104 +1,106 @@
 // from chain-space folder command prompt  -  $ browserify src/main.js -o app.js
 
 var $ = require('jquery');
-var swatches = require('./swatches');
-var d3 = require("d3");
+var d3 = require('d3');
 var parser = require('./parser');
 var interpreter = require('./interpreter');
+var swatchlist = require('./swatches');
+var stitchlist = require('./stitches');
 var p = new parser.Parser();
+var swatches = swatchlist.swatches;
+var stitches = stitchlist.stitches;
 
 var main = function(){
-    "use strict";
+    'use strict';
+
+    // set-up formatting
+    $('.full-height').height($('.main').parent().height());
+    $('#pattern-text').height($('.left-column').height()/2);
+    $('.right-column').children().toggle(false);
+    $('#info').toggle(true);
+    $('#stitches').toggle(false);
+
+    //activate event listeners
     $('#logo').click(function(){
 	location.reload(true);
     });
-    
-    $('.right-column').children().toggle(false);
-    $('#info').toggle(true);
-    
-    $(".full-height").height($(".main").parent().height());
 
-    $("#faq-button").click(function(){
+    $('.button-bar').children().click(function(){
 	$('.right-column').children().toggle(false);
-	$('#faq').toggle(true);
-    });
-    $("#info-button").click(function(){
-	$('.right-column').children().toggle(false);
-	$('#info').toggle(true);
-    });
-    $("#about-button").click(function(){
-	$('.right-column').children().toggle(false);
-	$('#about').toggle(true);
+	//activate the corresponding screen, depends on naming convention 'x-button' being the button for screen 'x'
+	var buttonid = this.id;
+	var screenid = '#' + buttonid.substring(0, buttonid.length-7);
+	$(screenid).toggle(true); 
     });
 
-    $("#granite").click(function(){
-	$("#pattern-text").val(swatches.granite);
+    $('#granite').click(function(){
+	$('#pattern-text').val(swatches[ 'granite']);
     });
-    $("#griddle").click(function(){
-	$("#pattern-text").val(swatches.griddle);
+    $('#griddle').click(function(){
+	$('#pattern-text').val(swatches[ 'griddle']);
     });
     
-    $("#create").click(function(){
-	var text = $("#pattern-text").val();
+    $('#create').click(function(){
+	var text = $('#pattern-text').val();
 	console.log(text);
 	var ast = p.parse(text);
-	console.log(ast);
+	//console.log(ast);
 	var pattern = interpreter.interpret(ast);
 	var nodes = pattern.stitches;
 	var links = pattern.connections;
-	console.log(nodes);
-	console.log(links);
+	//console.log(nodes);
+	//console.log(links);
+	var stitchtypes = {};
+	for (var key in nodes) {
+	    if (nodes[key].type in stitchtypes) { stitchtypes[nodes[key].type]++; }
+	    else { stitchtypes[nodes[key].type] = 0; }
+	};
+	//console.log(stitchtypes);
+	for (var key in stitchtypes) {
+	    $('#stitches-list').append('<li><div style="display: inline-block;"><span> ' + key + '</span><div style="display: inline-block;"><svg height="30" width="30"><g transform="translate(15,15)">' +stitches[key] + '</g></svg></div>' + '</div> </li>');
+	};
+	$('#pattern-text').height($('.left-column').height()/3);
+	$('#stitches').toggle(true);
 	
 	//Create svg frame
 	$('.right-column').children().toggle(false);
 	$('#chart').toggle(true);
-	var frame = d3.select("#frame");
+	var frame = d3.select('#frame');
 
 	// Create chart (force layout)
 	var chart = d3.layout.force()
 	    .nodes(nodes) 
 	    .links(links) 
-	    .size([$("#chart").width(), $("#chart").height()])
+	    .size([$('#chart').width(), $('#chart').height()])
 	    .linkDistance(30)
 	    .charge(-60)
-	    .on("tick", tick); //calculate movement
+	    .on('tick', tick); //calculate movement
 
 	chart.start();
-	console.log(nodes);
-	console.log(links);
-	var link = frame.selectAll(".link") // converting links to svg lines
+	//console.log(nodes);
+	//console.log(links);
+	var link = frame.selectAll('.link') // converting links to svg lines
 	    .data(links)
-	    .enter().append("line")
-	    .attr("class", "link");
-	console.log(link);
+	    .enter().append('line')
+	    .attr('class', 'link');
+	//console.log(link);
 
-	var node = frame.selectAll(".node") // converting nodes to g blocks 
+	var node = frame.selectAll('.node') // converting nodes to g blocks 
 	    .data(nodes)
-	    .enter().append("g")
-	    .html( function(d) {return chooseStitch(d.type)})
-	    .attr("class", "node")
+	    .enter().append('g')
+	    .html( function(d) {return stitches[d.type]})
+	    .attr('class', 'node')
 	    .call(chart.drag);
-
-	function chooseStitch(st) {//given string, returns appropriate g html
-	    switch(st) {
-	    case "ch":
-		return '<ellipse class="stitch" type="ch" cx=0 cy=0 rx=8 ry=4> </g>';
-	    case "sc":
-		return '<g class="stitch" type="sc"> <path class="scpath1" d="m 0,-10 0,20" /> <path class="scpath2" d="m -7,0 14,0" /> </g>';
-	    case "hdc":
-		return '<g class="stitch" type="hdc"> <path class="hdcpath1" d="m 0,0 0,20"/> <path class="hdcpath2" d="m -7,0 14,0" /> </g>';
-	    }
-	};
 	
 	function tick() {    
-	    frame.selectAll(".link")
-		.attr("x1", function(d) { return d.source.x; })
-		.attr("y1", function(d) { return d.source.y; })
-		.attr("x2", function(d) { return d.target.x; })
-		.attr("y2", function(d) { return d.target.y; });	
+	    frame.selectAll('.link')
+		.attr('x1', function(d) { return d.source.x; })
+		.attr('y1', function(d) { return d.source.y; })
+		.attr('x2', function(d) { return d.target.x; })
+		.attr('y2', function(d) { return d.target.y; });	
 
-	    frame.selectAll(".node")
-		.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+	    frame.selectAll('.node')
+		.attr('transform', function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 	}
     });
 };
--- src/swatches.js
+++ src/swatches.js
@@ -1,3 +1,6 @@
+var swatches = {
+    granite:'Row 1: ch, ch, ch, ch, ch, ch, ch, ch, ch. \nRow 2: ch, sc, sc, sc, sc, sc, sc, sc, sc, sc. \nRow 3: ch, sc, sk, ch, sc, sk, ch, sc, sk, ch, sc, sk, ch, sc. \nRow 4: ch, sc, sc, sk, ch, sc, sk, ch, sc, sk, ch, sc, sc.',
+    griddle:'Row 1: ch, ch, ch, ch, ch, ch, ch, ch, ch. \nRow 2: ch, hdc, sc, hdc, sc, hdc, sc, hdc, sc, hdc. \nRow 3: ch, sc, hdc, sc, hdc, sc, hdc, sc, hdc, sc. \nRow 4: ch, hdc, sc, hdc, sc, hdc, sc, hdc, sc, hdc.'
+};
 
-module.exports.granite ='Row 1: ch, ch, ch, ch, ch, ch, ch, ch, ch. \nRow 2: ch, sc, sc, sc, sc, sc, sc, sc, sc, sc. \nRow 3: ch, sc, sk, ch, sc, sk, ch, sc, sk, ch, sc, sk, ch, sc. \nRow 4: ch, sc, sc, sk, ch, sc, sk, ch, sc, sk, ch, sc, sc.';
-module.exports.griddle ='Row 1: ch, ch, ch, ch, ch, ch, ch, ch, ch. \nRow 2: ch, hdc, sc, hdc, sc, hdc, sc, hdc, sc, hdc. \nRow 3: ch, sc, hdc, sc, hdc, sc, hdc, sc, hdc, sc. \nRow 4: ch, hdc, sc, hdc, sc, hdc, sc, hdc, sc, hdc.';
+module.exports.swatches = swatches;
